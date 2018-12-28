e01d9e00671226e97a6fe18a0b23339a8d1c8f56
Cameron Grams <cameron.grams.2@gmail.com>
2018-12-28 17:46:48 -0500

    build the real one

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,23 +6,29 @@ const Card = ( props ) => {
     const individualCardStyle ={
         border: '1px solid green',
         margin: '20px',
-        padding: '10px',
+        padding: '0px',
         width: '500px'
     }
 
     const videoCardDiv = {
-        width: "100%",
-        height: "100%"
+        width: "90%",
+        height: "90%",
+        margin: "auto",
+    }
+
+    const otherInfoDiv = {
+
     }
 
     return(
         <div style={ individualCardStyle }>
             <div style={ videoCardDiv } >
-                <ReactPlayer url={ props.cardUrl } playing ></ReactPlayer> 
+                <ReactPlayer url={ props.cardUrl } playing={ true } width={ "100%" } controls ></ReactPlayer> 
             </div>
-             
-            <h3>{ props.title }</h3>
-            <p>{ props.content }</p>
+            <div style={ otherInfoDiv } > 
+                <h3>{ props.title }</h3>
+                <p>{ props.content }</p>
+            </div>
         </div>
     )
 }
